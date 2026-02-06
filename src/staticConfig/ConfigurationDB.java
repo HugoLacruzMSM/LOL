bf793cde06f5404bc9b0672a5e9ac6aa44b58033
HUGO LACRUZ HARO <hugo@msm.local>
2026-02-06 08:16:31 +0100

    COURSE: Refactored some clases

---
+++ src/staticConfig/ConfigurationDB.java
@@ -0,0 +1,8 @@
+package staticConfig;
+
+public class ConfigurationDB {
+    public static void main(String[] args) {
+        System.out.println("URL");
+        System.out.println("US");
+    }
+}
